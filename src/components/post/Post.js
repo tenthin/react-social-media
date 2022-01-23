70af0c444d9046e4fbde75dc5fc7c5d6f873b3cf
Tenzin <tenthinten@gmail.com>
2022-01-23 21:06:17 +0530

    fist post

---
+++ src/components/post/Post.js
@@ -0,0 +1,30 @@
+import "./post.css"
+import { MoreVert } from "@material-ui/icons"
+
+export default function Post() {
+    return (
+        <div className="post">
+            <div className="postWrapper">
+                <div className="postTop">
+                    <div className="postTopLeft">
+                        <img 
+                            className="postProfileImg" 
+                            src="/assets/person/1.jpeg" 
+                            alt="" 
+                        />
+                        <span className="postUsername">Tenzin Thinley</span>
+                        <span className="postDate">5 mins ago</span>
+                    </div>
+                    <div className="postTopRight">
+                        <MoreVert/>
+                    </div>
+                </div>
+                <div className="postCenter">
+                    <span className="postText">Hey! its my first post</span>
+                    <img src="/assets/post/1.jpeg" alt="" />
+                </div>
+                <div className="postBottom"></div>
+            </div>
+        </div>
+    )
+}
