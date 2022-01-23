16c3e5a035b36bc431661084b0c96dbd58168d28
Tenzin <tenthinten@gmail.com>
2022-01-23 21:28:04 +0530

    right bar modify

--- src/components/rightbar/Rightbar.js
+++ src/components/rightbar/Rightbar.js
@@ -3,7 +3,15 @@ import "./rightbar.css"
 export default function Rightbar() {
     return (
         <div className="rightbar">
-            rightbar
+            <div className="rightbarWrapper">
+                <div className="birthdayContainer">
+                    <img className="birthdayImg" src="assets/gift.png" alt="" />
+                    <span className="birthdayText">
+                        {" "}
+                        <b>Pola Foster</b> and <b>3 other friends</b> have a birthday today
+                    </span>
+                </div>
+            </div>
         </div>
     )
 }
