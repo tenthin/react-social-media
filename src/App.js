9fe602c8233afa7e6299e74fc42d4dd6d383e176
Tenzin <tenthinten@gmail.com>
2022-01-25 18:06:38 +0530

    completed

--- src/App.js
+++ src/App.js
@@ -1,11 +1,7 @@
 import Home from "./pages/home/Home";
-import Login from "./pages/login/Login";
-import Profile from "./pages/profile/Profile";
-import Register from "./pages/register/Register";
-// import {Person} from '@material-ui/icons'
 
 function App() {
-  return <Register/>
+  return <Home/>
 }
 
 export default App;
--- src/pages/register/Register.js
+++ src/pages/register/Register.js
@@ -12,8 +12,10 @@ export default function Register() {
                 </div>
                 <div className="loginRight">
                     <div className="loginBox">
+                        <input placeholder="Username" className="loginInput" />
                         <input placeholder="Email" className="loginInput" />
                         <input placeholder="Password" className="loginInput" />
+                        <input placeholder="Password Again" className="loginInput" />
                         <button className="loginButton">Sign Up</button>
                         <button className="loginRegisterButton">
                             Log into your account
