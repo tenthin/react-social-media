4fc5182463df63bdb4343733e1c007fbebf44099
Tenzin <tenthinten@gmail.com>
2022-01-25 15:14:13 +0530

    cover and profile imgs at their position

--- src/pages/profile/Profile.js
+++ src/pages/profile/Profile.js
@@ -11,7 +11,16 @@ export default function Profile() {
             <div className="profile">
                 <Sidebar/>
                 <div className="profileRight">
-                    <div className="profileRightTop">afsgsfgs</div>
+                    <div className="profileRightTop">
+                        <div className="profileCover">
+                            <img className="profileCoverImg" src="assets/post/3.jpeg" alt="" />
+                            <img className="profileUserImg" src="assets/person/7.jpeg" alt="" />
+                        </div>
+                        <div className="profileInfo">
+                            <h4 className="profileInfoName">Tenzin Thinley</h4>
+                            <span className="profileInfoDesc">Hello My friends</span>
+                        </div>
+                    </div>
                     <div className="profileRightBottom">
                         <Feed/>
                         <Rightbar/>
