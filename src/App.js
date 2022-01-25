dccd2925d3ee7b11b3efa7fce8f17267f23d5e4b
Tenzin <tenthinten@gmail.com>
2022-01-25 15:04:08 +0530

    create profile pic

--- src/App.js
+++ src/App.js
@@ -1,8 +1,9 @@
 import Home from "./pages/home/Home";
+import Profile from "./pages/profile/Profile";
 // import {Person} from '@material-ui/icons'
 
 function App() {
-  return <Home/>
+  return <Profile/>
 }
 
 export default App;
