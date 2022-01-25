63fa3b9ecd74dc9fcb3f06806ce8759d9dd4592f
Tenzin <tenthinten@gmail.com>
2022-01-25 21:32:06 +0530

    link home page with profile page

--- src/components/topbar/Topbar.js
+++ src/components/topbar/Topbar.js
@@ -1,12 +1,14 @@
 import "./topbar.css"
 import {Search,Person,Chat,Notifications} from "@material-ui/icons"
+import {Link} from "react-router-dom"
 
 export default function Topbar() {
   return (
     <div className="topbarContainer">
-
         <div className="topbarLeft">
-            <span className="logo">GatherZone</span>
+            <Link to="/" style={{textDecoration:"none"}}>
+                <span className="logo">GatherZone</span>
+            </Link>
         </div>
 
         <div className="topbarCenter">
--- src/pages/profile/Profile.js
+++ src/pages/profile/Profile.js
@@ -5,6 +5,8 @@ import Feed from "../../components/feed/Feed";
 import Rightbar from "../../components/rightbar/Rightbar";
 
 export default function Profile() {
+const PF = process.env.REACT_APP_PUBLIC_FOLDER;
+
     return (
         <>
             <Topbar/>
@@ -13,8 +15,16 @@ export default function Profile() {
                 <div className="profileRight">
                     <div className="profileRightTop">
                         <div className="profileCover">
-                            <img className="profileCoverImg" src="assets/post/3.jpeg" alt="" />
-                            <img className="profileUserImg" src="assets/person/7.jpeg" alt="" />
+                            <img 
+                                className="profileCoverImg" 
+                                src={`${PF}post/3.jpeg`}
+                                alt="" 
+                            />
+                            <img 
+                                className="profileUserImg" 
+                                src={`${PF}person/7.jpeg`} 
+                                alt="" 
+                            />
                         </div>
                         <div className="profileInfo">
                             <h4 className="profileInfoName">Tenzin Thinley</h4>
